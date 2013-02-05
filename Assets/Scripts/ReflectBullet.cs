8a48d5aaaec87ac070831e732d926109defbbfeb
J Cmon <scaraba@gmail.com>
2013-02-04 20:00:00 -0800

    bulletReflect

--- Assets/Scripts/ReflectBullet.cs
+++ Assets/Scripts/ReflectBullet.cs
@@ -7,14 +7,20 @@ public class ReflectBullet : Bullet {
 
     public override void setDirection(Vector3 dir)
     {
-       m_bullet1.m_fireDir.y = m_bullet1.m_fireDir.y * -1;
-       m_bullet1.m_fireDir.x = m_bullet1.m_fireDir.x * -1;
+        m_bullet1.m_fireDir = dir;
+       //m_bullet1.m_fireDir.y = m_bullet1.m_fireDir.y * -1;
+       //m_bullet1.m_fireDir.x = m_bullet1.m_fireDir.x * -1;
+    }
+
+    void Start()
+    {
+        reflectBullet(m_bullet1);
     }
 
 
     void reflectBullet(Bullet b)
     {
-
+        b.m_Damage = m_bullet1.m_Damage;
         b.gameObject.tag = gameObject.tag;
     }
 
